136a14aec552004c6d90d59693e4dac17ac20912
gigahacker6769 <kyryl.semenchuk@myivyis.org>
2026-09-15 14:51:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,31 +3,31 @@ const games = [
     title: "OvO Platformer",
     category: "Action",
     image: "https://images.unsplash.com/photo-1550745165-9bc0b252726f?w=400",
-    embedUrl: "https://scratch.mit.edu/projects/630225139/embed"
+    embedUrl: "https://turbowarp.org/630225139/embed"
   },
   {
     title: "Geometry Dash Lite",
     category: "Arcade",
     image: "https://images.unsplash.com/photo-1511512578047-dfb367046420?w=400",
-    embedUrl: "https://scratch.mit.edu/projects/105500895/embed"
+    embedUrl: "https://turbowarp.org/105500895/embed"
   },
   {
     title: "Pac-Man Classic",
     category: "Arcade",
     image: "https://images.unsplash.com/photo-1579373903781-fd5c0c30c4cd?w=400",
-    embedUrl: "https://scratch.mit.edu/projects/10128407/embed"
+    embedUrl: "https://turbowarp.org/10128407/embed"
   },
   {
     title: "Space Shooters",
     category: "Action",
     image: "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?w=400",
-    embedUrl: "https://scratch.mit.edu/projects/10015822/embed"
+    embedUrl: "https://turbowarp.org/10015822/embed"
   },
   {
     title: "Flappy Bird",
     category: "Arcade",
     image: "https://images.unsplash.com/photo-1534423861386-85a16f5d13fd?w=400",
-    embedUrl: "https://scratch.mit.edu/projects/18381211/embed"
+    embedUrl: "https://turbowarp.org/18381211/embed"
   }
 ];
 
